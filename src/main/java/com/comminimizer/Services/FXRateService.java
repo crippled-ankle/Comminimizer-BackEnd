0b19fb1798b8cc8293e37c2dc207c001756bd6ce
crippled-ankle <pengchenuw@gmail.com>
2020-08-30 15:55:22 -0400

    Isolate out the FX Rate Retrieval Service

---
+++ src/main/java/com/comminimizer/Services/FXRateService.java
@@ -0,0 +1,74 @@
+package com.comminimizer.Services;
+
+import com.comminimizer.Query.FXQuery;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class FXRateService {
+    public class fxRate {
+        Double rate;
+        java.util.Date lastUpdatedTime;
+
+        public fxRate(Double rate) {
+            this.rate = rate;
+            this.lastUpdatedTime = new Date();
+        }
+    }
+
+    Map<String, fxRate> fxRateCache = new HashMap<>();
+    static final String FX_RATE_QUERY_URL_PREFIX = "ADD_HERE";
+    static final String FX_RATE_QUERY_URL_SUFFIX = "ADD_HERE";
+
+    public Double downloadRate(FXQuery q) {
+        //fetch rate from Bank of Canada API
+        String seriesName = "FX" + q.originCode + q.destinationCode;
+        String url = FX_RATE_QUERY_URL_PREFIX + seriesName.replace("\"", "") + FX_RATE_QUERY_URL_SUFFIX;
+        RestTemplate rt = new RestTemplate();
+        Double rate = -1.0;
+        try {
+            String result = rt.getForObject(url, String.class);
+            JsonParser jp = new JsonParser();
+            JsonElement je = jp.parse(result);
+            rate = je.getAsJsonObject().getAsJsonArray("observations").get(0).getAsJsonObject().getAsJsonObject(seriesName).get("v").getAsDouble();
+        } catch (Exception ex) {
+            System.out.println("FX rate not available: " + q.originCode + "->" + q.destinationCode + " " + ex);
+        }
+        fxRate r = new fxRate(rate);
+        fxRateCache.put(q.getPairIden(), r);
+        return rate;
+    }
+
+    public Double getRate(FXQuery q) {
+        String cp = q.getPairIden();
+        Double rate = -1.0;
+        if (q.originCode.equals(q.destinationCode)) {
+            rate = 1.0;
+            return rate;
+        }
+        System.out.println(fxRateCache.containsKey(q.getPairIden()));
+        if(fxRateCache.containsKey(cp)){
+            Date current = new Date();
+            Calendar c = Calendar.getInstance();
+            fxRate r = fxRateCache.get(cp);
+            c.setTime(r.lastUpdatedTime);
+            c.add(Calendar.DATE, 1);
+            Date nextUpdate = c.getTime();
+            if(current.before(nextUpdate)) {
+                rate = r.rate;
+            } else {
+                rate = downloadRate(q);
+            }
+        } else {
+            rate = downloadRate(q);
+        }
+        return rate;
+    }
+}
